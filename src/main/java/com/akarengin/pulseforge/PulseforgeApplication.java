936ed7ce5e79d9d6631620830877cd5e6c512b74
akarengin <engin.akar1@gmail.com>
2025-12-24 18:05:47 +0300

    chore: spring boot 4.0 template

---
+++ src/main/java/com/akarengin/pulseforge/PulseforgeApplication.java
@@ -0,0 +1,13 @@
+package com.akarengin.pulseforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseforgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseforgeApplication.class, args);
+    }
+
+}
